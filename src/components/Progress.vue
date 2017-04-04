b8ae4bb4b27fd3464f7e81fc78653088d424a2f5
fsarachu <fsarachu@gmail.com>
2017-04-03 22:04:27 -0300

    Add progress bar component

---
+++ src/components/Progress.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3 class="title is-4 has-text-centered">Quotes added: {{ current }} / {{ max }}</h3>
+        <progress class="progress is-primary" :value="current" :max="max"></progress>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            max: {
+                type: Number,
+                required: true
+            },
+            current: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+</script>
