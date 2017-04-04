83d7529d7fc611151cc47f238bd884557a0d6496
fsarachu <fsarachu@gmail.com>
2017-04-03 22:26:34 -0300

    Add AddQuote module

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
     <div id="app">
         <app-progress :current="quoteCount" :max="maxQuotes"></app-progress>
+        <hr>
+        <app-add-quote></app-add-quote>
     </div>
 </template>
 
 <script>
+    import {bus} from './main';
     import AppProgress from './components/Progress.vue';
+    import AppAddQuote from './components/AddQuote.vue';
 
     export default {
         components: {
-            AppProgress
+            AppProgress,
+            AppAddQuote
         },
         data: function () {
             return {
@@ -17,10 +22,20 @@
                 maxQuotes: 10
             }
         },
+        methods: {
+            addQuote(quote) {
+                if (this.quotes.length < this.maxQuotes) {
+                    this.quotes.push(quote);
+                }
+            }
+        },
         computed: {
             quoteCount() {
                 return this.quotes.length;
             }
+        },
+        mounted() {
+            bus.$on('quoteAdded', this.addQuote);
         }
     }
 </script>
---
+++ src/components/AddQuote.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="field">
+            <label class="label">Add your quote:</label>
+            <p class="control">
+                <textarea v-model="quote" ref="textarea" class="textarea" placeholder="Some inspiring quote..." autofocus></textarea>
+            </p>
+        </div>
+        <div class="field is-grouped">
+            <p class="control">
+                <button type="submit" @click="addQuote" class="button is-primary">Add Quote</button>
+            </p>
+            <p class="control">
+                <button @click="clearQuote" class="button is-link">Cancel</button>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {bus} from '../main';
+
+    export default {
+        data: function () {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            addQuote() {
+                bus.$emit('quoteAdded', this.quote);
+                this.clearQuote();
+                this.$refs.textarea.focus();
+            },
+            clearQuote() {
+                this.quote = '';
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
